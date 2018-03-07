08e81beed47dfbc54c287338259a78e826c217c9
Adam Levin <adamlevin@tutordelphia.com>
2018-03-07 11:13:29 -0500

    refactors homepage to use a template

--- grades/views.py
+++ grades/views.py
@@ -3,12 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse(
-        '''<html>
-                <head>
-                    <title>Gradebook</title>
-                </head>
-                <body>
-                </body>
-            </html>'''
-    ) 
+    return render(request, 'home.html')
